b2b2b306bdd6b9086a2aeb8c6b6b9f306d88482b
Shannon Watters <shannon.p.watters@protonmail.com>
2025-11-12 08:17:59 +0700

    init commit of neovim configuration

---
+++ init.lua
@@ -0,0 +1,45 @@
+-- ~/.config/nvim/init.lua
+
+-- Setup leader keys
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
+-- Setup user keymappings and settings
+require("swatters")
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+        }, 
+        true, 
+        {}
+      )
+      vim.fn.getchar()
+      os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- Import all plugin specs from lua/plugins/
+    { import = "plugins" },
+  },
+  install = { colorscheme = { "tokyonight" } },
+  checker = { enabled = true },
+})
+
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-treesitter/nvim-treesitter", 
+  branch = 'master', 
+  lazy = false, 
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "python", "javascript", "html", "css", "markdown", "markdown_inline", "make", "terraform", "yaml"},
+      highlight = { enable = true },
+      indent = { enable = true },
+      auto_install = true,
+    })
+  end,
+}
